a6b4260d43f25bd28e8fba42c4a20ce024db180d
chrisvl0252 <145122637+chrisvl0252@users.noreply.github.com>
2023-11-24 12:49:42 +0100

    commit

---
+++ JS/H1/H1O01.js
@@ -0,0 +1,29 @@
+// Chris Van Leeuwen
+
+function setup() {
+  canvas = createCanvas(1000,450);
+  background('grey');
+  canvas.parent('processing');
+  noLoop();
+}
+
+function draw() {
+  // groene cirkel zonder rand
+  
+  noStroke();
+  fill('green');
+  ellipse(225,220,390);
+  
+  // groene cirkel zonder rand
+  
+  noStroke();
+  fill('white');
+  ellipse(225,225,300);
+
+  // witte rechthoek met rode rand
+  
+  stroke('green');
+  fill('white');
+  strokeWeight(10);
+  rect(125,125,200,200);
+}
